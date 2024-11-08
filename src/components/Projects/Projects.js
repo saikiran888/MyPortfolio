7f67fb5e31994766fc24f64fff325c3172a99dcb
Saikiran <34718070+saikiran888@users.noreply.github.com>
2024-11-08 17:03:01 -0500

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import ChatwithPdf from "../../Assets/Projects/ChatwithPdf.png";
+import CustomerChurn from "../../Assets/Projects/CustomerChurn.png";
+import Multilanguage_invoice from "../../Assets/Projects/Multilanguage_invoice.png";
+import Nextword_predict from "../../Assets/Projects/Nextword_predict.png";
+import Pneumonia_detection from "../../Assets/Projects/Pneumonia_detection.png";
+import StudentPerformance from "../../Assets/Projects/StudentPerformance.png";
 
 function Projects() {
   return (
@@ -23,68 +23,67 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={ChatwithPdf}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Chat with Pdf"
+              description="An end-to-end application for querying multiple PDF documents using Google Gemini Pro, Langchain, and Streamlit, with PDF upload, vector embedding processing, and interactive Q&A."
+              ghLink="https://github.com/saikiran888/Chat_with_Pdfs"
+              demoLink="https://chatwithpdfsllm.streamlit.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={StudentPerformance}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Student Performance Analysis"
+              description="A modular, end-to-end machine learning project following a real-world data science lifecycle. It covers data ingestion, preprocessing, model training, and deployment to predict math scores based on demographics and preparation factors.."
+              ghLink="https://github.com/saikiran888/mlproject"
+              // demoLink="https://github.com/saikiran888/mlproject"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={CustomerChurn}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Customer Churn Prediction"
+              description="A user-friendly Customer Churn Prediction App leveraging machine learning and ANN models to help businesses identify at-risk customers, visualize churn factors, and enhance retention strategies."
+              ghLink="https://github.com/saikiran888/Customer-Churn-Prediction"
+              demoLink="https://customer-churn-predictionapp.streamlit.app/"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={Multilanguage_invoice}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Multi-Language Invoice Extractor"
+              description="An advanced Multi-Language Invoice Extractor powered by Google Gemini Pro, providing accurate, efficient data extraction across multiple languages, with an interactive Q&A interface and seamless integration using Streamlit and Langchain."
+              ghLink="https://github.com/saikiran888/Multilanguage-Invoice-Extractor"
+              demoLink="https://multilanguage-invoice-imgextractor.streamlit.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={Pneumonia_detection}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="Pneumonia detection"
+              description="This project classifies pediatric CT scan images into Normal and Pneumonia categories using a DenseNet-161 model, achieving 92% accuracy with data augmentation to handle class imbalance."
+              ghLink="https://github.com/saikiran888/Pneumonia-Detection"
+              demoLink="https://drive.google.com/file/d/1Yy6IOMhbHXpcx2zFoQhlhc89AQ4SVfl8/view?pli=1"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={Nextword_predict}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="Next Word Prediction"
+              description="This Streamlit app predicts the next word in a sentence using a trained LSTM model and tokenizer, helping users complete their thoughts or explore AI-driven text generation."
+              ghLink="https://github.com/saikiran888/Next-Word-Prediction"
+              demoLink="https://next-word-predictionapp.streamlit.app/"
             />
           </Col>
         </Row>
